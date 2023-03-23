60d07021afd9f3ca404cd3088fd439aaddc348ae
jeromyzhang <zjdx1998@gmail.com>
2023-03-22 23:20:30 -0700

    merge from dev, add json

--- Engine/include/Core/GameObject.h
+++ Engine/include/Core/GameObject.h
@@ -10,16 +10,52 @@
 #include "Component.h"
 
 namespace CSPill::EngineCore {
-
+/**
+ * \brief Base class for all entities in CSPill Scenes.
+ *
+ * GameObject is the minimal unit to represent an object, it can be composed of multiple components.
+ */
 class GameObject{
  public:
+  /**
+   * Factory function to create an GameObject.
+   *
+   * @return a unique pointer to instantiated game object.
+   */
   static std::unique_ptr<GameObject> Create();
+  /**
+   * Virtual Destructor
+   */
   ~GameObject() = default;
 
+  /**
+   * Update is called every frame, if the object is enabled.
+   */
   void Update();
+
+  /**
+   * Render is called every frame, if the object is enabled.
+   * @param renderer SDL_Renderer renderer.
+   */
   void Render(SDL_Renderer* renderer);
+  /**
+   * Add a component to current GameObject.
+   * @param component new component that needs to be added.
+   * @return False if it's duplicated.
+   */
   bool AddComponent(std::unique_ptr<Component> component);
+
+  /**
+   * Remove a component based on the component name.
+   * @param component_name name of the component.
+   */
   void RemoveComponent(std::string_view component_name);
+
+  /**
+   * Get a raw pointer to component based on name.
+   * @param component_name name of the component
+   * @return Raw pointer of the targeted component.
+   */
   Component* GetComponent(std::string_view component_name);
 
  private:
