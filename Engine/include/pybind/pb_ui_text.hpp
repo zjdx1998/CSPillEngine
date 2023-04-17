3142adb795ceefbcf2f8c774ba0ccddb3da979f9
Jeromy Zhang <zjdx1998@gmail.com>
2023-04-17 09:11:44 +0000

    Committing clang-format changes

--- Engine/include/pybind/pb_ui_text.hpp
+++ Engine/include/pybind/pb_ui_text.hpp
@@ -8,7 +8,9 @@ namespace CSPill::EngineCore {
 
 void PB_UIText(py::module &m) {
   py::class_<UIText, GameObject, std::unique_ptr<UIText>>(m, "UIText")
-      .def_static("Create", &UIText::Create, py::arg("font_name"), py::arg("content"), py::arg("pos"), py::arg("size"), py::arg("font_size"))
+      .def_static("Create", &UIText::Create, py::arg("font_name"),
+                  py::arg("content"), py::arg("pos"), py::arg("size"),
+                  py::arg("font_size"))
       .def("GetFontName", &UIText::GetFontName)
       .def("SetFontName", &UIText::SetFontName)
       .def("GetPos", &UIText::GetPos)
@@ -19,10 +21,8 @@ void PB_UIText(py::module &m) {
       .def("SetSize", &UIText::SetSize)
       .def("GetContent", &UIText::GetContent)
       .def("SetContent", &UIText::SetContent)
-      .def(
-          "Render",
-          [](GameObject &self, void *temp) {
-            self.Render(static_cast<SDL_Renderer *>(temp));
-          });
+      .def("Render", [](GameObject &self, void *temp) {
+        self.Render(static_cast<SDL_Renderer *>(temp));
+      });
 }
 }  // namespace CSPill::EngineCore
--- Engine/src/Core/UIText.cpp
+++ Engine/src/Core/UIText.cpp
@@ -1,76 +1,71 @@
 #include "UIText.h"
-#include "TinyMath.hpp"
+
 #include <SDL_ttf.h>
+
 #include "ResourceManager.h"
+#include "TinyMath.hpp"
 
 namespace CSPill::EngineCore {
 
-std::unique_ptr<GameObject> UIText::Create(std::string_view font_name, std::string_view content, const CSPill::Math::Vec2D &pos, const CSPill::Math::Vec2D &size, int font_size) {
-  auto* ui_text = new UIText(font_name, content, pos, size, font_size);
+std::unique_ptr<GameObject> UIText::Create(std::string_view font_name,
+                                           std::string_view content,
+                                           const CSPill::Math::Vec2D &pos,
+                                           const CSPill::Math::Vec2D &size,
+                                           int font_size) {
+  auto *ui_text = new UIText(font_name, content, pos, size, font_size);
   return std::unique_ptr<GameObject>(ui_text);
 }
 
-UIText::UIText(std::string_view font_name, std::string_view content, const CSPill::Math::Vec2D &pos, const CSPill::Math::Vec2D &size, int font_size)
-    : GameObject(), font_name_(font_name), content_(content), pos_(pos), size_(size), font_size_(font_size), text_surface_(nullptr), text_texture_(nullptr) {}
+UIText::UIText(std::string_view font_name, std::string_view content,
+               const CSPill::Math::Vec2D &pos, const CSPill::Math::Vec2D &size,
+               int font_size)
+    : GameObject(),
+      font_name_(font_name),
+      content_(content),
+      pos_(pos),
+      size_(size),
+      font_size_(font_size),
+      text_surface_(nullptr),
+      text_texture_(nullptr) {}
 
-const std::string &UIText::GetFontName() const {
-    return font_name_;
-}
+const std::string &UIText::GetFontName() const { return font_name_; }
 
 void UIText::SetFontName(const std::string &font_name) {
-    font_name_ = font_name;
+  font_name_ = font_name;
 }
 
-int UIText::GetFontSize() const {
-    return font_size_;
-}
+int UIText::GetFontSize() const { return font_size_; }
 
-void UIText::SetFontSize(int font_size) {
-    font_size_ = font_size;
-}
+void UIText::SetFontSize(int font_size) { font_size_ = font_size; }
 
-const CSPill::Math::Vec2D &UIText::GetSize() const {
-    return size_;
-}
+const CSPill::Math::Vec2D &UIText::GetSize() const { return size_; }
 
-void UIText::SetSize(const CSPill::Math::Vec2D &size) {
-    size_ = size;
-}
+void UIText::SetSize(const CSPill::Math::Vec2D &size) { size_ = size; }
 
-const CSPill::Math::Vec2D &UIText::GetPos() const {
-    return pos_;
-}
+const CSPill::Math::Vec2D &UIText::GetPos() const { return pos_; }
 
-void UIText::SetPos(const CSPill::Math::Vec2D &pos) {
-    pos_ = pos;
-}
+void UIText::SetPos(const CSPill::Math::Vec2D &pos) { pos_ = pos; }
 
-const std::string &UIText::GetContent() const {
-    return content_;
-}
+const std::string &UIText::GetContent() const { return content_; }
 
-void UIText::SetContent(const std::string &content) {
-    content_ = content;
-}
+void UIText::SetContent(const std::string &content) { content_ = content; }
 
 SDL_FRect UIText::GetBoundingBox() {
   return {pos_.x - size_.x / 2, pos_.y - size_.y / 2, size_.x, size_.y};
 }
 
 void UIText::Render(SDL_Renderer *renderer) {
-    // Load a font
-    TTF_Font *font = ResourceManager::GetInstance().LoadFont(font_name_);
-    TTF_SetFontSize(font, font_size_);
-    SDL_Color color = {255, 255, 255};
-    text_surface_ = TTF_RenderText_Solid(font, content_.c_str(), color);
-    text_texture_ = SDL_CreateTextureFromSurface(renderer, text_surface_);
-    const auto rect = GetBoundingBox();
-    SDL_RenderCopyF(renderer, text_texture_, nullptr, &rect);
-    SDL_FreeSurface(text_surface_);
+  // Load a font
+  TTF_Font *font = ResourceManager::GetInstance().LoadFont(font_name_);
+  TTF_SetFontSize(font, font_size_);
+  SDL_Color color = {255, 255, 255};
+  text_surface_ = TTF_RenderText_Solid(font, content_.c_str(), color);
+  text_texture_ = SDL_CreateTextureFromSurface(renderer, text_surface_);
+  const auto rect = GetBoundingBox();
+  SDL_RenderCopyF(renderer, text_texture_, nullptr, &rect);
+  SDL_FreeSurface(text_surface_);
 }
 
-UIText::~UIText() {
-  SDL_DestroyTexture(text_texture_);
-}
+UIText::~UIText() { SDL_DestroyTexture(text_texture_); }
 
 }  // namespace CSPill::EngineCore
