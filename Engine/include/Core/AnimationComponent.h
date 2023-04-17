d4fa8050304e2a25a0bc227a08aa1d3da563309d
Jeromy Zhang <zjdx1998@gmail.com>
2023-04-17 11:00:04 +0000

    Committing clang-format changes

--- Engine/include/Core/AnimationComponent.h
+++ Engine/include/Core/AnimationComponent.h
@@ -8,29 +8,29 @@
 
 namespace CSPill::EngineCore {
 
-    class AnimationComponent : public Component {
-    public:
-        explicit AnimationComponent(const std::string_view& name);
-        AnimationComponent(AnimationComponent&&) = default;
-        void Update(GameObject* object, float dt) override;
-        void Render(SDL_Renderer* renderer) override;
-        std::unordered_map<std::string, std::vector<std::string>> GetAnimations()
-            const;
-        void AddAnimation(const std::string& name,
-            const std::vector<std::string>& animation);
-        void RemoveAnimation(const std::string& name);
-        std::string_view GetCurrentAnimation() const;
-        void SetCurrentAnimation(const std::string& name);
-        int GetCurrentAnimationFrame() const;
-        void SetCurrentAnimationFrame(int frame);
+class AnimationComponent : public Component {
+ public:
+  explicit AnimationComponent(const std::string_view& name);
+  AnimationComponent(AnimationComponent&&) = default;
+  void Update(GameObject* object, float dt) override;
+  void Render(SDL_Renderer* renderer) override;
+  std::unordered_map<std::string, std::vector<std::string>> GetAnimations()
+      const;
+  void AddAnimation(const std::string& name,
+                    const std::vector<std::string>& animation);
+  void RemoveAnimation(const std::string& name);
+  std::string_view GetCurrentAnimation() const;
+  void SetCurrentAnimation(const std::string& name);
+  int GetCurrentAnimationFrame() const;
+  void SetCurrentAnimationFrame(int frame);
 
-    private:
-        std::unordered_map<std::string, std::vector<std::string>> animations_;
-        std::string current_animation_;
-        int current_animation_frame_ = 0;
-        SDL_Rect mSrc;
-        SDL_Rect mDest;
-    };
+ private:
+  std::unordered_map<std::string, std::vector<std::string>> animations_;
+  std::string current_animation_;
+  int current_animation_frame_ = 0;
+  SDL_Rect mSrc;
+  SDL_Rect mDest;
+};
 
 }  // namespace CSPill::EngineCore
 
