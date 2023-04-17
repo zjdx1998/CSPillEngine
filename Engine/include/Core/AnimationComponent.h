473a40ba5bd8334ff9dde172c68c096870b8e634
DailyGrow <858519155@qq.com>
2023-04-17 03:59:15 -0700

    add Animation Component

---
+++ Engine/include/Core/AnimationComponent.h
@@ -0,0 +1,37 @@
+#ifndef CSPILLENGINE_ENGINE_INCLUDE_CORE_ANIMATION_COMPONENT_H_
+#define CSPILLENGINE_ENGINE_INCLUDE_CORE_ANIMATION_COMPONENT_H_
+
+#include <vector>
+
+#include "GameObject.h"
+#include "TinyMath.hpp"
+
+namespace CSPill::EngineCore {
+
+    class AnimationComponent : public Component {
+    public:
+        explicit AnimationComponent(const std::string_view& name);
+        AnimationComponent(AnimationComponent&&) = default;
+        void Update(GameObject* object, float dt) override;
+        void Render(SDL_Renderer* renderer) override;
+        std::unordered_map<std::string, std::vector<std::string>> GetAnimations()
+            const;
+        void AddAnimation(const std::string& name,
+            const std::vector<std::string>& animation);
+        void RemoveAnimation(const std::string& name);
+        std::string_view GetCurrentAnimation() const;
+        void SetCurrentAnimation(const std::string& name);
+        int GetCurrentAnimationFrame() const;
+        void SetCurrentAnimationFrame(int frame);
+
+    private:
+        std::unordered_map<std::string, std::vector<std::string>> animations_;
+        std::string current_animation_;
+        int current_animation_frame_ = 0;
+        SDL_Rect mSrc;
+        SDL_Rect mDest;
+    };
+
+}  // namespace CSPill::EngineCore
+
+#endif  // CSPILLENGINE_ENGINE_INCLUDE_CORE_ANIMATION_COMPONENT_H_
---
+++ Engine/src/Core/AnimationComponent.cpp
@@ -0,0 +1,64 @@
+#include "AnimationComponent.h"
+
+#include "ResourceManager.h"
+#include "TransformComponent.h"
+
+namespace CSPill::EngineCore {
+
+    void AnimationComponent::Update(GameObject* object, float dt) {
+        current_animation_frame_ = dt;
+
+        if (current_animation_frame_ > 10) {
+            current_animation_frame_ = 0;
+        }
+
+        mSrc.x = current_animation_frame_ * 75;
+        mSrc.y = 0;
+        mSrc.w = 128;
+        mSrc.h = 128;
+        auto transform_component =
+            (TransformComponent*)(object->GetComponent("TransformComponent"));
+        Math::Vec2D pos = transform_component->position();
+        mDest.x = pos.x;
+        mDest.y = pos.y;
+        mDest.w = 128;
+        mDest.h = 128;
+    }
+    void AnimationComponent::Render(SDL_Renderer* renderer) {
+        std::string query =
+            current_animation_ + "-" + std::to_string(current_animation_frame_);
+        SDL_Texture* mTexture = ResourceManager::GetInstance().QueryTexture(query);
+        SDL_RenderCopy(renderer, mTexture, &mSrc, &mDest);
+    }
+    std::unordered_map<std::string, std::vector<std::string>>
+        AnimationComponent::GetAnimations() const {
+        return animations_;
+    }
+    void AnimationComponent::AddAnimation(
+        const std::string& name, const std::vector<std::string>& animation) {
+        animations_[name] = animation;
+    }
+    void AnimationComponent::RemoveAnimation(const std::string& name) {
+        for (auto it = animations_.begin(); it != animations_.end(); it++) {
+            if (it->first.compare(name)) {
+                animations_.erase(it);
+                break;
+            }
+        }
+    }
+    std::string_view AnimationComponent::GetCurrentAnimation() const {
+        return current_animation_;
+    }
+    void AnimationComponent::SetCurrentAnimation(const std::string& name) {
+        current_animation_ = name;
+    }
+    int AnimationComponent::GetCurrentAnimationFrame() const {
+        return current_animation_frame_;
+    }
+    void AnimationComponent::SetCurrentAnimationFrame(int frame) {
+        current_animation_frame_ = frame;
+    }
+    AnimationComponent::AnimationComponent(const std::string_view& name)
+        : Component(name), current_animation_frame_(0) {}
+
+}  // namespace CSPill::EngineCore
