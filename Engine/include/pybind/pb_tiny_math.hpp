fb71d9e7d460d3b7c381879035cd9329cba6da4b
Jeromy Zhang <zjdx1998@gmail.com>
2023-04-17 00:40:38 +0000

    Committing clang-format changes

--- Engine/include/pybind/pb_tiny_math.hpp
+++ Engine/include/pybind/pb_tiny_math.hpp
@@ -1,5 +1,5 @@
-#include <pybind11/pybind11.h>
 #include <pybind11/operators.h>
+#include <pybind11/pybind11.h>
 
 #include "TinyMath.hpp"
 
@@ -9,26 +9,30 @@ namespace CSPill::Math {
 
 void PB_TinyMath(py::module &m) {
   py::class_<Vec2D>(m, "Vec2D")
-        .def(py::init<float, float, float>(), py::arg("x") = 0, py::arg("y") = 0, py::arg("w") = 0)
-        .def("__getitem__", [](const Vec2D &v, int index) { return v[index]; })
-        .def("__setitem__", [](Vec2D &v, int index, float value) { v[index] = value; })
-        .def(py::self *= float())
-        .def(py::self /= float())
-        .def(py::self += py::self)
-        .def(py::self -= py::self)
-        .def(py::self * py::self)
-        .def(py::self * float())
-        .def(py::self / float())
-        .def("__eq__", &Vec2D::operator==)
-        .def("magnitude", &Vec2D::Magnitude)
-        .def("dot", &Vec2D::Dot)
-        .def("normalize", &Vec2D::Normalize)
-        .def("project", &Vec2D::Project)
-        .def("cross_product", &Vec2D::CrossProduct)
-        .def("__add__", &Vec2D::operator+)
-        .def("__sub__", &Vec2D::operator-)
-        .def("__str__", [](const Vec2D &v) {
-            return "Vec2D(x: " + std::to_string(v.x) + ", y: " + std::to_string(v.y) + ", w: " + std::to_string(v.w) + ")";
-        });
+      .def(py::init<float, float, float>(), py::arg("x") = 0, py::arg("y") = 0,
+           py::arg("w") = 0)
+      .def("__getitem__", [](const Vec2D &v, int index) { return v[index]; })
+      .def("__setitem__",
+           [](Vec2D &v, int index, float value) { v[index] = value; })
+      .def(py::self *= float())
+      .def(py::self /= float())
+      .def(py::self += py::self)
+      .def(py::self -= py::self)
+      .def(py::self * py::self)
+      .def(py::self * float())
+      .def(py::self / float())
+      .def("__eq__", &Vec2D::operator==)
+      .def("magnitude", &Vec2D::Magnitude)
+      .def("dot", &Vec2D::Dot)
+      .def("normalize", &Vec2D::Normalize)
+      .def("project", &Vec2D::Project)
+      .def("cross_product", &Vec2D::CrossProduct)
+      .def("__add__", &Vec2D::operator+)
+      .def("__sub__", &Vec2D::operator-)
+      .def("__str__", [](const Vec2D &v) {
+        return "Vec2D(x: " + std::to_string(v.x) +
+               ", y: " + std::to_string(v.y) + ", w: " + std::to_string(v.w) +
+               ")";
+      });
 }
-}  // namespace CSPill::EngineCore
+}  // namespace CSPill::Math
